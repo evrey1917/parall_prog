f756b5c80430dc3d4ff59c801a4cefc167905db3
evrey1917 <143984979+evrey1917@users.noreply.github.com>
2024-03-01 11:08:55 +0700

    Update sin_sum.cpp

--- sin_sum.cpp
+++ sin_sum.cpp
@@ -10,7 +10,7 @@
 
 int main()
 {
-    NAME pi = acos(-1), sum_of_sinus = 0, n = 10000000, delta = 1/n, sin_arg = 0, sin_out;
+    NAME pi = acos(-1), sum_of_sinus = 0, sum_of_sinus_array = 0, n = 10000000, delta = 1/n, sin_arg = 0, sin_out;
 
     std::vector<NAME> vector_NAME;
 
@@ -19,11 +19,14 @@ int main()
         sin_out = sin(2*pi*sin_arg);
         vector_NAME.push_back(sin_out);
 
+        sum_of_sinus_array = sum_of_sinus_array + vector_NAME[i];
         sum_of_sinus = sum_of_sinus + sin_out;
-        sin_arg = sin_arg + delta;
+
+        sin_arg = delta * i;
     }
 
     std::cout << typeid(sum_of_sinus).name() << " sum of sinus in [0; 2pi]: " << sum_of_sinus << std::endl;
+    std::cout << typeid(sum_of_sinus).name() << " sum of sinus in [0; 2pi] from array: " << sum_of_sinus << std::endl;
 
     return 0;
 }
